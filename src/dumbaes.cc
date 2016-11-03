f851a8170317a3533fd52acd58418e317f99b838
Michael Catanzaro <michael.catanzaro@mst.edu>
2016-11-02 21:42:31 -0500

    Add API skeleton
    Let's have a data type for a 128-bit block of data.
    
    Let's have a data type for that 4x4 AES state grid.
    
    Let's have API for encrypting one block. It's a stub for now and making
    it work is basically the entire project.
    
    Once it is working, we have to add CBC on top, but that should be
    straightforward.

---
+++ src/dumbaes.cc
@@ -0,0 +1,63 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (c) 2016 Michael Catanzaro <michael.catanzaro@mst.edu>
+ * Copyright (c) 2016 Jacob Fischer <jtf3m8@mst.edu>
+ * Copyright (c) 2016 Christian Storer <cs9yb@mst.edu>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "dumbaes.h"
+
+#include <utility>
+
+namespace dumbaes {
+
+// The AES algorithm state is a 4x4 table.
+using State = std::array<std::array<uint8_t, 4>, 4>;
+
+static State block_to_state(const Block& block)
+{
+    State state;
+    for (unsigned i = 0; i < 4; i++)
+        for (unsigned j = 0; j < 4; j++)
+            state[i][j] = block[i + 4*j];
+    return state;
+}
+
+static Block state_to_block(State&& state)
+{
+    Block block;
+    for (unsigned i = 0; i < 4; i++)
+        for (unsigned j = 0; j < 4; j++)
+            block[i + 4*j] = state[i][j];
+    return block;
+}
+
+Block encrypt_block(const Block& block, const Block& key)
+{
+    State initial_state = block_to_state(block);
+    return state_to_block(std::move(initial_state));
+}
+
+}
---
+++ src/dumbaes.h
@@ -0,0 +1,47 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (c) 2016 Michael Catanzaro <michael.catanzaro@mst.edu>
+ * Copyright (c) 2016 Jacob Fischer <jtf3m8@mst.edu>
+ * Copyright (c) 2016 Christian Storer <cs9yb@mst.edu>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include <array>
+#include <cstdint>
+
+namespace dumbaes {
+
+// Note: 16 bytes * 8 bits/byte = 128 bits
+using Block = std::array<uint8_t, 16>;
+
+/**
+ * It encrypts one block of data with the specified key. This probably shouldn't
+ * ever be used directly by applications; it's intended for use by block cipher
+ * mode implementations.
+ */
+Block encrypt_block(const Block& block, const Block& key);
+
+}
